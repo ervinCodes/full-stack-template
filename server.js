022399f415b4b6e46f7efca71af47d12cf15ba61
Ervin Natareno <ean1207@gmail.com>
2022-08-11 14:20:01 -0700

    Template Complete

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+app.use(express.static("public"));
 
 // ========================
 // Listen
