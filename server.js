75e477c78f41be91836098f049d9d217b2f561a3
Ervin Natareno <ean1207@gmail.com>
2022-08-11 13:22:39 -0700

    Added Middleware

--- server.js
+++ server.js
@@ -4,13 +4,29 @@ const cors = require("cors");
 const MongoClient = require("mongodb").MongoClient;
 require("dotenv").config();
 
-let db;
-(dbConnectionString = process.env.DB_STRING),
-  (dbName = "sample_mflix"),
+let db,
+  dbConnectionString = process.env.DB_STRING,
+  dbName = "sample_mflix",
   collection;
 
 MongoClient.connect(dbConnectionString).then((client) => {
   console.log("Connected to Database");
   db = client.db(dbName);
   collection = db.collection("movies");
+});
+
+// ========================
+// Middlewares
+// ========================
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+// ========================
+// Listen
+// ========================
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server is running on port 9000`);
 });
